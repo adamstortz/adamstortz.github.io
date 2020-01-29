99530085d63278a9e2b8a967b99940669756fa47
Adam Stortx <adam.stortz@live.com>
2020-01-28 18:10:04 -0600

    Updates

---
+++ precache-manifest.184a3104083ce52642327fe86c6bc595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3080de687c4a725ddc8005958dc644f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cea4ff72a43531072cd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0dbfd6c76c1a5dd922b5",
+    "url": "/static/js/2.615746f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.615746f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cea4ff72a43531072cd",
+    "url": "/static/js/main.6318a8ab.chunk.js"
+  },
+  {
+    "revision": "a0ab25cfd0733e04fd3a",
+    "url": "/static/js/runtime-main.c885ee43.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
