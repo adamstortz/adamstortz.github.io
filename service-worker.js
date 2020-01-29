9f5f50a005e9640f1f0c874ac71f0c97d2d7a0b9
adamstortz <astortz@redstonecontentsolutions.com>
2020-01-29 00:41:45 +0000

    Deploying to master from  - 2a2e5ccc27578cea1dac70ab96fc30c8116a4b4e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.184a3104083ce52642327fe86c6bc595.js"
+  "/precache-manifest.2fdf20143274aa43518b64f0429ba0ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
