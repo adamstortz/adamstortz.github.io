1127ff3bcbe6135d138db424fbfb3e94dbd67218
Jessica Stortz <jessicastortz@jessicas-mbp-2.lan>
2020-10-03 21:46:45 -0500

    minor fix

--- libs/ui/src/lib/name-value/name-value.spec.tsx
+++ libs/ui/src/lib/name-value/name-value.spec.tsx
@@ -5,7 +5,7 @@ import NameValue from './name-value';
 
 describe('NameValue', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<NameValue key="a" value="b" />);
+    const { baseElement } = render(<NameValue name="a" value="b" />);
     expect(baseElement).toBeTruthy();
   });
 });
